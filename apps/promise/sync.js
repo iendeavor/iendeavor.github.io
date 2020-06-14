45cfc586310974890cb282c1fdcb1fb1f5257abb
Ernest <iattempt.net@gmail.com>
2020-06-14 21:02:43 +0800

    fix: wrong flow

--- apps/promise/sync.js
+++ apps/promise/sync.js
@@ -1,7 +1,12 @@
 let accessToken = window.localStorage.getItem('accessToken')
 
-if (accessToken === null) {
+if (location.searchParams.get('code')) {
   console.log(1)
+  // handle redirect
+  window.localStorage.setItem('accessToken', location.url.searchParams.get('code'))
+  location.reload()
+} else if (accessToken === null) {
+  console.log(2)
   // to be auth
   const query = [
     'client_id=4755cf31f5cbc4ad47cf',
@@ -10,11 +15,6 @@ if (accessToken === null) {
   const url = `https://github.com/login/oauth/authorize?${query}`
   document.getElementById('auth').setAttribute('href', url)
   document.getElementById('auth').classList.remove('hidden')
-} else if (location.searchParams.get('code')) {
-  console.log(2)
-  // handle redirect
-  window.localStorage.setItem('accessToken', location.url.searchParams.get('code'))
-  location.reload()
 } else {
   console.log(3)
   fetch('https://api.github.com/user')
